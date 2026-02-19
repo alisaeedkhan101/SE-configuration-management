c36d39a52947a7bac69560595adfb1560549dbd8
Sikandarh11 <sikandarnust1140@gmail.com>
2026-02-19 14:53:04 +0500

    Add Ameer Hamza to the attendance list

--- attendance.py
+++ attendance.py
@@ -1,4 +1,4 @@
-students = ['alisaeed', "Sikandar Hussain 405410", "Rizwan Saddique 432229"]
+students = ['alisaeed', "Sikandar Hussain 405410", "Rizwan Saddique 432229", "Ameer Hamza 409662" ]
 
 def show_attendance():
     print("Class Attendance List:")
