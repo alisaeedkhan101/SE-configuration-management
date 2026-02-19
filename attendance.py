5eb9249919c83a59bda3e68fb5ad82dfb0f85501
ris001 <ris001@anthilla.com>
2026-02-19 12:07:54 +0500

    Added attendance.py for classroom activity

---
+++ attendance.py
@@ -0,0 +1,9 @@
+students = []
+
+def show_attendance():
+    print("Class Attendance List:")
+    for student in students:
+        print("-", student)
+
+if __name__ == "__main__":
+    show_attendance()
